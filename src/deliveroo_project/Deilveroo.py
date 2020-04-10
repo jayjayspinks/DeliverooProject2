74bdc9b42b06fe96eb7d05b6452b0afcf62d2d40
Jay Jay <jayjayspinks@gmail.com>
2020-04-10 18:59:22 +0100

    added some formatting to change the size of the texts and number and buttons

--- src/deliveroo_project/Deilveroo.py
+++ src/deliveroo_project/Deilveroo.py
@@ -1,28 +1,28 @@
 import tkinter as tk
-
+from tkinter import *
 
 class Game(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
 
-        headline_text = tk.Label(self, text="Meals without a Deliveroo!!")
+        headline_text = tk.Label(self, text="Meals without a Deliveroo!!", font=("Ariel",144), fg="red")
         #headline_text.grid(column=2, row=0)
         headline_text.pack()
 
         self.label_var = tk.IntVar()
-        label = tk.Label(self, textvariable=self.label_var)  # Assigned that  variable to the label
+        label = tk.Label(self, textvariable=self.label_var, font=("Ariel",300), fg="red")  # Assigned that  variable to the label
         label.pack()
         #label.grid(column=2,row=1)
 
         deliveroo_button = tk.Button(self, text="Deliveroo",
-                                     relief="groove", command=self.reset_var)
+                                     relief="groove", command=self.reset_var, width=30, height=5)
         #deliveroo_button.grid(column=0, row=4)
-        deliveroo_button.pack()
+        deliveroo_button.pack(side=LEFT)
 
         not_deliveroo = tk.Button(self, text="Not Deliveroo!!",
-                           relief="groove", command=self.add_var)
+                           relief="groove", command=self.add_var, width=30, height=5)
         #not_deliveroo.grid(column=3, row=4)
-        not_deliveroo.pack()
+        not_deliveroo.pack(side=RIGHT)
 
     def add_var(self):
         self.label_var.set(self.label_var.get() + 1)
@@ -33,7 +33,7 @@ class Game(tk.Frame):
 root = tk.Tk()
 root.title("Meals Without Deliveroo!!")
 root.resizable()
-root.geometry("250x100")
+root.geometry("2500x1000")
 game = Game(root)
 game.pack()
 root.mainloop()

dd87b415828226e30a8e967f0a30457350f6a3f1
Jay Jay <jayjayspinks@gmail.com>
2020-04-10 17:50:17 +0100

    added redundnt button with an empty function and label displaying variable

--- src/deliveroo_project/Deilveroo.py
+++ src/deliveroo_project/Deilveroo.py
@@ -1,4 +1,15 @@
 import tkinter as tk
+from tkinter import *
+
+#def incrementNotDeliverooCounter():
+
+
+counter = 0
+
+def incrementNotDeliveroo():
+    return 
+
+
 window = tk.Tk()
 window.title("My first GUI")
 window.geometry("300x200")
@@ -9,10 +20,10 @@ headline_text.grid(column=2, row=0)
 deliveroo_button = tk.Button (window, text="Deliveroo")
 deliveroo_button.grid(column=0,row=4)
 
-deliveroo_button = tk.Button (window, text="Not Deliveroo")
+deliveroo_button = tk.Button (window, text="Not Deliveroo",command=incrementNotDeliveroo)
 deliveroo_button.grid(column=3,row=4)
 
-days_count = tk.Label(text=0)
+days_count = tk.Label(text=counter)
 days_count.grid(column=2,row=1)
 
 window.mainloop()

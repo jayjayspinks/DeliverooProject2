5802d9ff5cea1849e0a0e9fc5a4ad7f57c544a99
Jay Jay <jayjayspinks@gmail.com>
2020-04-10 18:44:53 +0100

    completely redesigned code. functionality working but all bodged.

--- src/deliveroo_project/Deilveroo.py
+++ src/deliveroo_project/Deilveroo.py
@@ -1,32 +1,39 @@
 import tkinter as tk
-from tkinter import *
-
-#def incrementNotDeliverooCounter():
 
 
-counter = 0
+class Game(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
 
-def incrementNotDeliveroo():
-    return
+        headline_text = tk.Label(self, text="Meals without a Deliveroo!!")
+        #headline_text.grid(column=2, row=0)
+        headline_text.pack()
 
-def resetCounter():
-    return
+        self.label_var = tk.IntVar()
+        label = tk.Label(self, textvariable=self.label_var)  # Assigned that  variable to the label
+        label.pack()
+        #label.grid(column=2,row=1)
 
+        deliveroo_button = tk.Button(self, text="Deliveroo",
+                                     relief="groove", command=self.reset_var)
+        #deliveroo_button.grid(column=0, row=4)
+        deliveroo_button.pack()
 
-window = tk.Tk()
-window.title("My first GUI")
-window.geometry("300x200")
+        not_deliveroo = tk.Button(self, text="Not Deliveroo!!",
+                           relief="groove", command=self.add_var)
+        #not_deliveroo.grid(column=3, row=4)
+        not_deliveroo.pack()
 
-headline_text = tk.Label(text="Meals without a Deliveroo!!")
-headline_text.grid(column=2, row=0)
+    def add_var(self):
+        self.label_var.set(self.label_var.get() + 1)
 
-deliveroo_button = tk.Button (window, text="Deliveroo", command=resetCounter)
-deliveroo_button.grid(column=0,row=4)
+    def reset_var(self):
+        self.label_var.set(0)
 
-deliveroo_button = tk.Button (window, text="Not Deliveroo", command=incrementNotDeliveroo)
-deliveroo_button.grid(column=3,row=4)
-
-days_count = tk.Label(text=counter)
-days_count.grid(column=2,row=1)
-
-window.mainloop()
+root = tk.Tk()
+root.title("Meals Without Deliveroo!!")
+root.resizable()
+root.geometry("250x100")
+game = Game(root)
+game.pack()
+root.mainloop()

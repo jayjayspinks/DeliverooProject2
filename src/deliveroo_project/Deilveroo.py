a94ff333d01ceed427c7b26778715d427c407fc5
Jay Jay <jayjayspinks@gmail.com>
2020-04-10 17:07:26 +0100

    test commit

--- src/deliveroo_project/Deilveroo.py
+++ src/deliveroo_project/Deilveroo.py
@@ -2,4 +2,12 @@ import tkinter as tk
 window = tk.Tk()
 window.title("My first GUI")
 window.geometry("300x200")
+
+headline_text = tk.Label(text="Meals without a Deliveroo!! test")
+headline_text.grid(column=0, row=0)
+
+
+days_count = tk.Label(text=0)
+days_count.grid(column=0,row=1)
+
 window.mainloop()

79003302c432237ebc77919f71481c5f94a5f11a
Jay Jay <jayjayspinks@gmail.com>
2020-04-10 17:20:02 +0100

    added redundnt buttons, changed layout a bit

--- src/deliveroo_project/Deilveroo.py
+++ src/deliveroo_project/Deilveroo.py
@@ -4,10 +4,15 @@ window.title("My first GUI")
 window.geometry("300x200")
 
 headline_text = tk.Label(text="Meals without a Deliveroo!!")
-headline_text.grid(column=0, row=0)
+headline_text.grid(column=2, row=0)
 
+deliveroo_button = tk.Button (window, text="Deliveroo")
+deliveroo_button.grid(column=0,row=4)
+
+deliveroo_button = tk.Button (window, text="Not Deliveroo")
+deliveroo_button.grid(column=3,row=4)
 
 days_count = tk.Label(text=0)
-days_count.grid(column=0,row=1)
+days_count.grid(column=2,row=1)
 
 window.mainloop()

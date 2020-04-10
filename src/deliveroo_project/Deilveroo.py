97bf4e33eb67003573b70f9ac34cf7b04a5b5dca
Jay Jay <jayjayspinks@gmail.com>
2020-04-10 17:51:40 +0100

    added empty function for 2nd button

--- src/deliveroo_project/Deilveroo.py
+++ src/deliveroo_project/Deilveroo.py
@@ -7,7 +7,10 @@ from tkinter import *
 counter = 0
 
 def incrementNotDeliveroo():
-    return 
+    return
+
+def resetCounter():
+    return
 
 
 window = tk.Tk()
@@ -17,10 +20,10 @@ window.geometry("300x200")
 headline_text = tk.Label(text="Meals without a Deliveroo!!")
 headline_text.grid(column=2, row=0)
 
-deliveroo_button = tk.Button (window, text="Deliveroo")
+deliveroo_button = tk.Button (window, text="Deliveroo", command=resetCounter)
 deliveroo_button.grid(column=0,row=4)
 
-deliveroo_button = tk.Button (window, text="Not Deliveroo",command=incrementNotDeliveroo)
+deliveroo_button = tk.Button (window, text="Not Deliveroo", command=incrementNotDeliveroo)
 deliveroo_button.grid(column=3,row=4)
 
 days_count = tk.Label(text=counter)
